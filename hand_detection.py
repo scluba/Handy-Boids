12df4f92141cccd81b8ccf211f21d5a36f777a51
scluba <arseniysherstnev@gmail.com>
2025-07-30 17:21:05 -0700

    Simplified hand detection and central attraction logic.

--- hand_detection.py
+++ hand_detection.py
@@ -1,6 +1,6 @@
 import cv2, random
 import numpy as np
-#import mediapipe as mp
+import mediapipe as mp
 from boids import Boid
 from scipy.spatial.transform import Rotation
 
@@ -9,19 +9,19 @@ WIDTH, HEIGHT = None, None
 
 class Hands:
 
-    def __init__(self, max_num_hands, min_detection_confidence, smoothing_alpha):
-        #self.mp_hands = mp.solutions.hands
-        #self.mp_draw = mp.solutions.drawing_utils
-        #self.hands = self.mp_hands.Hands(max_num_hands = max_num_hands, min_detection_confidence = min_detection_confidence)
+    def __init__(self, max_num_hands, min_detection_confidence,):
+        self.mp_hands = mp.solutions.hands
+        self.mp_draw = mp.solutions.drawing_utils
+        self.hands = self.mp_hands.Hands(max_num_hands = max_num_hands, min_detection_confidence = min_detection_confidence)
         self.finger_names = {'Thumb': 1, 'Index': 2, 'Middle': 3, 'Ring': 4, 'Pinky': 5}
         self.finger_inds = {name: list(range(4 * i - 3, 4 * i + 1)) for name, i in self.finger_names.items()}
-        self.smoothed_angles, self.rot_offset = np.zeros(3), np.zeros(3)
-        self.smoothing_alpha = smoothing_alpha
+        self.rot_offset = np.zeros(3)
         self.boids = [Boid(random.randint(0, 1920), random.randint(0, 1080), 1920, 1080) for _ in range(100)]
         self.img, self.landmarks = None, None
 
     # Return the appropriate coordinate of node
     def _node_point(self, node: int, frmt: str):
+        #print(node)
         match frmt:
             case 'x':
                 return self.landmarks[node].x
@@ -53,7 +53,8 @@ class Hands:
         fingers = []
 
         for nodes, ind in self.finger_inds.items():
-            root, tip = nodes[1], nodes[3]
+            #print(f'Nodes: {ind}')
+            root, tip = ind[1], ind[3]
             fingers.append('Down' if self._node_point(root, 'y') < self._node_point(tip, 'y') else 'Up')
         
         return fingers
@@ -105,13 +106,13 @@ class Hands:
 
     # Return the distance between two fingertips
     def inter_finger_dist(self, finger_1: str, finger_2: str):
-        root, tip = self.finger_inds[finger_1][-1], self.finger_inds[finger_2][-1]
+        root, tip = self._node_point(self.finger_inds[finger_1][-1], 'xy'), self._node_point(self.finger_inds[finger_2][-1], 'xy')
 
-        root *= [WIDTH, HEIGHT]
-        tip *= [WIDTH, HEIGHT]
+        root *= np.array([WIDTH, HEIGHT])
+        tip *= np.array([WIDTH, HEIGHT])
         dist = np.linalg.norm(tip - root)
 
-        all_points = np.array([self._node_point(i, 'xy') * [WIDTH, HEIGHT] for name, i in self.finger_inds])
+        all_points = np.array([self._node_point(i, 'xy') * np.array([WIDTH, HEIGHT]) for i in range(0, 21)])
         x_min, y_min = np.min(all_points, axis = 0)
         x_max, y_max = np.max(all_points, axis = 0)
 
@@ -120,12 +121,31 @@ class Hands:
 
         return norm_dist, dist, tuple(root), tuple(tip)
     
+    # Display datapoints on-screen
+    def _display_set(self, text: list, vals: list, offset: int):
+        for ind, pack in enumerate(zip(text, vals)):
+            txt = f'{pack[0]}: {pack[1]}'
+            cv2.putText(self.img, txt, (10, offset + 20 * ind), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 0), 2)
+    
+    # Debug Menu
+    # Visualize all hand nodes, finger/curl stats, hand rotation, and boid modifiers
+    def _debug(self, finger_states, curl_states, hand_rotation, hand_landmarks, boid_modifiers):
+        finger_data = [f'{state}, {deg}' for state, deg in zip(finger_states, curl_states.values())]
+        self._display_set(list(self.finger_names.keys()), finger_data, 30)
+        self._display_set(['Yaw', 'Pitch', 'Roll'], hand_rotation, 150)
+        self._display_set(['Perception', 'Alignment Coef.', 'Attraction', 'Speed'], boid_modifiers, 240)
+
+        self.rotating_cube(hand_rotation)
+
+        self.mp_draw.draw_landmarks(self.img, hand_landmarks, self.mp_hands.HAND_CONNECTIONS)
+    
     def run(self):
-        cap = cv2.VideoCapture()
+        cap = cv2.VideoCapture(1)
         global WIDTH, HEIGHT
         WIDTH, HEIGHT = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
         perception, norm_pinch, speed = 0, 0, 0
-        center = np.array([0, 0])
+        attraction, touching = True, False
+        center, hand_rotation = np.zeros(2), np.zeros(3)
 
         while True:
             ret, self.img = cap.read()
@@ -137,15 +157,49 @@ class Hands:
 
             if results.multi_hand_landmarks:
                 for hand_landmarks in results.multi_hand_landmarks:
-                    self.mp_draw.draw_landmarks(self.img, hand_landmarks, self.mp_hands.HAND_CONNECTIONS)
-                    self.landmarks = hand_landmarks.landmarks
+                    self.landmarks = hand_landmarks.landmark
 
                     finger_states = self.finger_direction()
                     curl_states = {name: self.calculate_finger_angle(name) for name, i in self.finger_names.items()}
                     hand_rotation = self.hand_rotation()
 
+                    roll = hand_rotation[2]
+                    ti_dist, _, _, _ = self.inter_finger_dist('Thumb', 'Index')
+                    tm_dist, _, _, _ = self.inter_finger_dist('Thumb', 'Middle')
+                    center = np.array([self._node_point(9, 'xy') * [WIDTH, HEIGHT]])
+
+                    speed = np.clip(np.interp(curl_states['Ring'], [90, 180], [4.0, 8.0]), 4.0, 8.0)
+                    alignment = np.clip(np.interp(ti_dist, [0.05, 0.7], [0.0, 2.0]), 0.0, 2.0)
+                    if tm_dist < 0.1:
+                        if not touching:
+                            attraction = not attraction
+                            touching = True
+                    else:
+                        touching = False
+                    perception = np.clip(np.interp(roll, [-180, 90], [100, 250]), 100, 250)
+
+                    global DEBUG
+                    if DEBUG: self._debug(finger_states, curl_states, hand_rotation, hand_landmarks,[perception, f'{ti_dist}, {alignment}', f'{tm_dist}, {attraction}', speed])
+
+            for boid in self.boids:
+                boid.perception = perception
+                boid.max_speed = speed
+                boid.flock(self.boids, align_w = norm_pinch)
+                if attraction and results.multi_hand_landmarks: boid.attract(center[0], strength = 0.75)
+                boid.update()
+                boid.draw(self.img)
+
+            cv2.imshow('Hand Controlled Boids', self.img)
+
+            key = cv2.waitKey(1) & 0xFF
+            if key == ord('q'): break
+            elif key == ord('d'): DEBUG = not DEBUG
+            elif key == ord('r'):
+                self.rot_offset = hand_rotation
+
+        cap.release()
+        cv2.destroyAllWindows()
 
 
-
-h = Hands(1, 2, 3)
-h.calculate_finger_angle('Thumb')
+h = Hands(1, 0.7)
+h.run()
